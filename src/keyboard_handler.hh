9aa593bc2803e5f8799cd34b7d4a45b9e7b955c1
Nicolas Blin <nblin@lrde.epita.fr>
2021-07-02 15:00:09 +0200

    [add][exit] add exit button on F1

--- src/keyboard_handler.hh
+++ src/keyboard_handler.hh
@@ -20,31 +20,40 @@ struct KeyboardHandler
         delta = current_frame - last_frame;
         last_frame = current_frame;
 
-        float camera_speed = std::min(camera.base_speed * delta.count(), camera.base_speed * 0.05f);
+        float camera_speed = std::min(camera.base_speed * delta.count(),
+                                      camera.base_speed * 0.05f);
 
-        switch(key)
+        switch (key)
         {
-            case 101:
-                camera.origin_ += camera_speed * camera.target_;
-                break;
-            case 103:
-                camera.origin_ -= camera_speed * camera.target_;
-                break;
-            case 100:
-                camera.origin_ -= glm::normalize(glm::cross(camera.target_, camera.up_)) * camera_speed;
-                break;
-            case 102:
-                camera.origin_ += glm::normalize(glm::cross(camera.target_, camera.up_)) * camera_speed;
-                break;    
-            default:
-                return;
+        case GLUT_KEY_UP:
+            camera.origin_ += camera_speed * camera.target_;
+            break;
+        case GLUT_KEY_DOWN:
+            camera.origin_ -= camera_speed * camera.target_;
+            break;
+        case GLUT_KEY_LEFT:
+            camera.origin_ -=
+                glm::normalize(glm::cross(camera.target_, camera.up_)) *
+                camera_speed;
+            break;
+        case GLUT_KEY_RIGHT:
+            camera.origin_ +=
+                glm::normalize(glm::cross(camera.target_, camera.up_)) *
+                camera_speed;
+            break;
+        case GLUT_KEY_F1:
+            glutDestroyWindow(glutGetWindow());
+            exit(0);
+            break;
+        default:
+            return;
         }
 
         camera.update_camera(camera);
         glutPostRedisplay();
     }
-
 };
 
-std::chrono::time_point<std::chrono::system_clock> KeyboardHandler::last_frame = std::chrono::system_clock::now();
+std::chrono::time_point<std::chrono::system_clock> KeyboardHandler::last_frame =
+    std::chrono::system_clock::now();
 std::chrono::duration<float> KeyboardHandler::delta;
